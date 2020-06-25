5d15c5d090084ea5a2569cf90c0d269435320936
Shahzaib <shahzaibux@gmail.com>
2020-06-26 03:26:25 +0500

    Project Initiaied

---
+++ admin/category.php
@@ -0,0 +1,15 @@
+<?php include "header.php"; ?>
+<div id="admin-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <h1 class="admin-heading">All Categories</h1>
+            </div>
+            <div class="col-md-2">
+                <a class="add-new" href="add-category.php">add category</a>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<?php include "footer.php"; ?>
