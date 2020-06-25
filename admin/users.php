1b54d4c26ff5795abbbfb4b0f7655f2ae519090e
Shahzaib <shahzaibux@gmail.com>
2020-06-26 03:57:25 +0500

    Project Initiaied

---
+++ admin/users.php
@@ -0,0 +1,15 @@
+<?php include "header.php"; ?>
+  <div id="admin-content">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-10">
+                  <h1 class="admin-heading">All Users</h1>
+              </div>
+              <div class="col-md-2">
+                  <a class="add-new" href="add-user.php">add user</a>
+              </div>
+              
+          </div>
+      </div>
+  </div>
+<?php include "header.php"; ?>
