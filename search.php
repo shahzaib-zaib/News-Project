7e7605271154e9446085e2efdd4d25b6592cb500
Shahzaib <shahzaibux@gmail.com>
2020-06-26 16:37:14 +0500

    Updated Project

---
+++ search.php
@@ -0,0 +1,21 @@
+<?php include 'header.php'; ?>
+    <div id="main-content">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- post-container -->
+                <div class="post-container">
+                  <h2 class="page-heading">Search : Search Term</h2>
+                    
+                    <ul class='pagination'>
+                        <li class="active"><a href="">1</a></li>
+                        <li><a href="">2</a></li>
+                        <li><a href="">3</a></li>
+                    </ul>
+                </div><!-- /post-container -->
+            </div>
+            <?php include 'sidebar.php'; ?>
+        </div>
+      </div>
+    </div>
+<?php include 'footer.php'; ?>
