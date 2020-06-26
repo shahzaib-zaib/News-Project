4a074048eb69f151af008c7e96e564d0ed0a4314
Shahzaib <shahzaibux@gmail.com>
2020-06-26 18:53:17 +0500

    Updated Project

--- sidebar.php
+++ sidebar.php
@@ -15,7 +15,23 @@
     <!-- recent posts box -->
     <div class="recent-post-container">
         <h4>Recent Posts</h4>
-        
+        <div class="recent-post">
+            <a class="post-img" href="">
+                <img src="images/post-format.jpg" alt=""/>
+            </a>
+            <div class="post-content">
+                <h5><a href="single.php">Lorem ipsum dolor sit amet</a></h5>
+                <span>
+                    <i class="fa fa-tags" aria-hidden="true"></i>
+                    <a href='category.php'>Html</a>
+                </span>
+                <span>
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                    01 Nov, 2019
+                </span>
+                <a class="read-more" href="single.php">read more</a>
+            </div>
+        </div>
     </div>
     <!-- /recent posts box -->
 </div>
