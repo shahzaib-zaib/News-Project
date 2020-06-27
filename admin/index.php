f881854e6a7db33025467823a183a97a7e6e10b9
Shahzaib <shahzaibux@gmail.com>
2020-06-28 01:45:42 +0500

    Updated Project

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,13 @@
+<?php
+    include "config.php";
+
+    session_start();
+
+    if(isset($_SESSION["username"])){
+        header("Location: {$hostname}/admin/post.php");
+    }
+
+?>
 <!doctype html>
 <html>
    <head>
