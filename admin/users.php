fc35daf84c7c6bcb561436b1f566e219976edc1b
Shahzaib <shahzaibux@gmail.com>
2020-06-27 14:59:32 +0500

    Updated Project

--- admin/users.php
+++ admin/users.php
@@ -11,8 +11,17 @@
               <div class="col-md-12">
                 <?php
                     include "config.php";
+                    $limit = 3;
 
-                    $sql = "SELECT * FROM user ORDER BY user_id DESC";
+                    
+                    if(isset($_GET['page'])){
+                        $page = $_GET['page'];
+                    }else{
+                        $page = 1;
+                    }
+                    $offset = ($page - 1) * $limit;
+
+                    $sql = "SELECT * FROM user ORDER BY user_id DESC LIMIT {$offset}, {$limit}";
                     $result = mysqli_query($con, $sql) or die ("Query Faild.");
                     if(mysqli_num_rows($result) > 0){
 
@@ -56,13 +65,27 @@
 
                     if(mysqli_num_rows($result1) > 0){
                         $total_record = mysqli_num_rows($result1);
-                        $limit = 3;
+                        
                         $total_pages = ceil($total_record / $limit);
 
                         echo "<ul class='pagination admin-pagination'>";
-                        for($i = 1; $i <= $total_pages; $i++){
-                            echo '<li><a href="users.php?page='. $i .'">'. $i .'</a></li>';
+                        if($page > 1){
+                            echo '<li><a href="users.php?page='.($page - 1).'">Prev</a></li>';
                         }
+                        
+                        for($i = 1; $i <= $total_pages; $i++){
+                            if ($i == $page) {
+                                $active = "active";
+                            } else {
+                                $active = "";
+                            }
+                            
+                            echo '<li class="'.$active.'"><a href="users.php?page='. $i .'">'. $i .'</a></li>';
+                        }
+                        if($total_pages > $page){
+                            echo '<li><a href="users.php?page='.($page + 1).'">Next</a></li>';
+                        }
+                        
                         echo "</ul>";
                     }
                   ?>
