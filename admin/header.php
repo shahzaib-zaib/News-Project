b1016dca3723d8054fad2bc7afb890b78fe4bc44
Shahzaib <shahzaibux@gmail.com>
2020-06-28 02:09:14 +0500

    Updated Project

--- admin/header.php
+++ admin/header.php
@@ -36,8 +36,8 @@
                     </div>
                     <!-- /LOGO -->
                       <!-- LOGO-Out -->
-                    <div class="col-md-offset-9  col-md-1">
-                        <a href="logout.php" class="admin-logout" >logout</a>
+                    <div class="col-md-offset-9 col-md-3">
+                        <a href="logout.php" class="admin-logout">Hello <?php echo $_SESSION["username"]; ?> logout</a>
                     </div>
                     <!-- /LOGO-Out -->
                 </div>
