e98292cf020b50af6b9e55f9722cc6612d4a1a03
Shahzaib <shahzaibux@gmail.com>
2020-06-28 01:41:34 +0500

    Updated Project

--- admin/header.php
+++ admin/header.php
@@ -1,3 +1,13 @@
+<?php
+    include "config.php";
+
+    session_start();
+
+    if(!isset($_SESSION["username"])){
+        header("Location: {$hostname}/admin/");
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
