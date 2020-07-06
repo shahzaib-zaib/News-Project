bd343e61b879ec25e71c263fb731099791b66aee
Shahzaib <shahzaibux@gmail.com>
2020-07-07 01:29:38 +0500

    Updated Project

--- admin/save-post.php
+++ admin/save-post.php
@@ -9,21 +9,34 @@
         $file_tmp = $_FILES['fileToUpload']['tmp_name'];
         $file_type = $_FILES['fileToUpload']['type'];
         $file_ext = strtolower(end(explode('.',$file_name)));
-
-
         $extentions = array("jpeg","jpg","png");
 
+        if(in_array($file_ext,$extentions) === false)
+        {
+            $errors[] = "This extention file not allowed, Please choose a JPG or PNG file";
+        }
 
-
-
+        if($file_size > 2097152){
+            $errors[] = "File size must be 2mb or lower.";
+        }
+        
+        if(empty($errors) == true){
+            move_uploaded_file($file_tmp,"upload/".$file_name);
+        }else{
+            print_r($errors);
+            die();
+        }
     }
 
     $title = mysqli_real_escape_string($con, $_POST['post_title']);
     $description = mysqli_real_escape_string($con, $_POST['postdesc']);
-    $title = mysqli_real_escape_string($con, $_POST['post_title']);
     $category = mysqli_real_escape_string($con, $_POST['category']);
     $date = date("d M Y");
     $author = $_SESSION['user_id'];
+
+
+    $sql = "INSERT INTO post(title, description, category,post_date, author, post_imp)
+            VALUES('{$title}', '{$description}', {$category}, '{$date}', {$author}, '{$file_name}')"
 
 
 ?>
