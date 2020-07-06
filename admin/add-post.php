8d45caa890122967ad273dd4c6937b6a9128ce47
Shahzaib <shahzaibux@gmail.com>
2020-07-07 00:39:21 +0500

    Updated Project

--- admin/add-post.php
+++ admin/add-post.php
@@ -19,7 +19,18 @@
                       <div class="form-group">
                           <label for="exampleInputPassword1">Category</label>
                           <select name="category" class="form-control">
-                              <option value="" selected> Select Category</option>
+                              <option disable> Select Category</option>
+                              <?php
+                                    include 'config.php';
+                                    $sql = "SELECT * FROM category";
+                                    $result = mysqli_query($con,$sql) or die("Query Failed");
+
+                                    if(mysqli_num_rows($result) > 0){
+                                        while($row = mysqli_fetch_assoc($result)){
+                                            echo "<option>{$row['category_name']}</option>";
+                                        }
+                                    }
+                            ?>
                           </select>
                       </div>
                       <div class="form-group">
