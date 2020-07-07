8b38e8b751f549f3e251b9441f0474c831af7340
Shahzaib <shahzaibux@gmail.com>
2020-07-08 04:04:57 +0500

    Updated Project

--- index.php
+++ index.php
@@ -31,11 +31,11 @@
                         <div class="post-content">
                             <div class="row">
                                 <div class="col-md-4">
-                                    <a class="post-img" href="single.php"><img src="admin/upload/<?php echo $row['post_img']; ?>" alt=""/></a>
+                                    <a class="post-img" href="single.php?id=<?php echo $row['post_id']; ?>"><img src="admin/upload/<?php echo $row['post_img']; ?>" alt=""/></a>
                                 </div>
                                 <div class="col-md-8">
                                     <div class="inner-content clearfix">
-                                        <h3><a href='single.php'><?php echo $row['title']; ?></a></h3>
+                                        <h3><a href='single.php?id=<?php echo $row['post_id']; ?>'><?php echo $row['title']; ?></a></h3>
                                         <div class="post-information">
                                             <span>
                                                 <i class="fa fa-tags" aria-hidden="true"></i>
@@ -51,7 +51,7 @@
                                             </span>
                                         </div>
                                         <p class="description"><?php echo $row['description']; ?></p>
-                                        <a class='read-more pull-right' href='single.php'>read more</a>
+                                        <a class='read-more pull-right' href='single.php?id=<?php echo $row['post_id'];?>'>read more</a>
                                     </div>
                                 </div>
                             </div>
--- single.php
+++ single.php
@@ -5,8 +5,21 @@
                 <div class="col-md-8">
                   <!-- post-container -->
                     <div class="post-container">
+                    <?php
+
+                            include "config.php";
+
+                            $sql = "SELECT post.post_id, post.title, post.description, post.post_date,
+                            category.category_name, user.username,post.category,post.post_img FROM post 
+                            LEFT JOIN category ON post.category = category.category_id
+                            LEFT JOIN user ON post.author = user.user_id";
+
+                            $result = mysqli_query($con, $sql) or die ("Query Faild.");
+                            if(mysqli_num_rows($result) > 0){
+                                while($row = mysqli_fetch_assoc($result)) {
+                        ?>
                         <div class="post-content single-post">
-                            <h3>Lorem ipsum dolor sit amet, consectetur</h3>
+                            <h3></h3>
                             <div class="post-information">
                                 <span>
                                     <i class="fa fa-tags" aria-hidden="true"></i>
@@ -26,6 +39,13 @@
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                             </p>
                         </div>
+                        <?php
+                                }
+                            }else{
+                                echo "<h2>No Record Found.</h2>";
+                            }
+
+                        ?>
                     </div>
                     <!-- /post-container -->
                 </div>
