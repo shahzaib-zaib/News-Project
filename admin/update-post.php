303fc520d24975a2c6c0d60bac55caccdbd3502e
Shahzaib <shahzaibux@gmail.com>
2020-07-08 00:39:09 +0500

    Updated Project

--- admin/update-post.php
+++ admin/update-post.php
@@ -16,8 +16,8 @@ if($_SESSION["user_role"] == '0'){
         include "config.php";
 
         $post_id = $_GET['id'];
-        $sql = "SELECT post.post_id, post.title, post.description, post.post_date,
-        category.category_name, user.username FROM post 
+        $sql = "SELECT post.post_id, post.title, post.description,post.post_img,
+        category.category_name FROM post 
         LEFT JOIN category ON post.category = category.category_id
         LEFT JOIN user ON post.author = user.user_id
         WHERE post.post_id = {$post_id}";
@@ -30,32 +30,41 @@ if($_SESSION["user_role"] == '0'){
         <!-- Form for show edit-->
         <form action="" method="POST" enctype="multipart/form-data" autocomplete="off">
             <div class="form-group">
-                <input type="hidden" name="post_id"  class="form-control" value="1" placeholder="">
+                <input type="hidden" name="post_id"  class="form-control" value="<?php echo $row['post_id']; ?>" placeholder="">
             </div>
             <div class="form-group">
                 <label for="exampleInputTile">Title</label>
-                <input type="text" name="post_title"  class="form-control" id="exampleInputUsername" value="Lorem ipsum dolor sit amet">
+                <input type="text" name="post_title"  class="form-control" id="exampleInputUsername" value="<?php echo $row['title']; ?>">
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1"> Description</label>
                 <textarea name="postdesc" class="form-control"  required rows="5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                <?php echo $row['description'] ?>
                 </textarea>
             </div>
             <div class="form-group">
                 <label for="exampleInputCategory">Category</label>
-                <select class="form-control" name="category">
-                    <option value="">Html</option>
-                    <option value="">Css</option>
-                    <option value="">javascript</option>
-                    <option value="">Python</option>
+                <select name="category" class="form-control">
+                    <option disable> Select Category</option>
+                    <?php
+                        include "config.php";
+                        $sql = "SELECT * FROM category";
+                        $result = mysqli_query($con,$sql) or die("Query Failed");
+
+                        if(mysqli_num_rows($result) > 0){
+                            while($row = mysqli_fetch_assoc($result)){
+                                
+                                echo "<option  value='{$row['category_id']}'>{$row['category_name']}</option>";
+                            }
+                        }
+                ?>
                 </select>
             </div>
             <div class="form-group">
                 <label for="">Post image</label>
                 <input type="file" name="new-image">
-                <img  src="upload/post_1.jpg" height="150px">
-                <input type="hidden" name="old-image" value="">
+                <img  src="upload/<?php echo $row['post_img'] ?>" height="150px">
+                <!-- <input type="hidden" name="old-image" value=""> -->
             </div>
             <input type="submit" name="submit" class="btn btn-primary" value="Update" />
         </form>
