bcc623e0c3128c16022553886939266c3a5f5c33
Shahzaib <shahzaibux@gmail.com>
2020-07-08 03:14:00 +0500

    Updated Project

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$hostname = "http://localhost:8080/news-project";
+
+$con = mysqli_connect("localhost","root","", "news-site") or die("Connection Failed : " . mysqli_connect_error());
+
+?>
