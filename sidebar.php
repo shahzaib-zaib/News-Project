04abe14f9758d8d03799606be07851508d2918d9
Shahzaib <shahzaibux@gmail.com>
2020-07-12 01:00:44 +0500

    Updated Project

--- sidebar.php
+++ sidebar.php
@@ -17,9 +17,26 @@
         <h4>Recent Posts</h4>
         <?php
 
-            
+        include "config.php";
 
+            /* Calculation Offset Code */
+            $limit = 3;
+            if(isset($_GET['page'])){
+                $page = $_GET['page'];
+            }else{
+                $page = 1;
+            }
+            $offset = ($page - 1) * $limit;
 
+            $sql = "SELECT post.post_id, post.title, post.description, post.post_date,post.author,
+            category.category_name, user.username,post.category,post.post_img FROM post 
+            LEFT JOIN category ON post.category = category.category_id
+            LEFT JOIN user ON post.author = user.user_id
+            ORDER BY post.post_id DESC LIMIT {$offset}, {$limit}";
+
+            $result = mysqli_query($con, $sql) or die ("Query Faild.");
+            if(mysqli_num_rows($result) > 0){
+                while($row = mysqli_fetch_assoc($result)) {
         ?>
         <div class="recent-post">
             <a class="post-img" href="">
