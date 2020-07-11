5ba738a60d495fa6a83acf1561942b9cab0b4054
Shahzaib <shahzaibux@gmail.com>
2020-07-12 01:51:26 +0500

    Updated Project

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>News</title>
+    <title><?php echo $pages[$page_name];?> | News</title>
     <!-- Bootstrap -->
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <!-- Font Awesome Icon -->
